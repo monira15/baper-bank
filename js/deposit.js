892a2cac70928d7ba38e3eb161771158f72447b7
monira15 <monira.programmer@gmail.com>
2023-08-15 16:58:27 +0600

    finised

--- js/deposit.js
+++ js/deposit.js
@@ -9,9 +9,21 @@
 const depositTotalElement = document.getElementById('deposit-total');
 const previousDepositTotalString = depositTotalElement.innerText;
 const previousDepositTotal = parseFloat(previousDepositTotalString);
-const currentDepositTotal = previousDepositTotal+NewDepositAmount;
+
+// step-4:
+const currentDepositTotal = previousDepositTotal + NewDepositAmount;
 
 depositTotalElement.innerText=currentDepositTotal;
+
+// step-5:get balance 
+const BalanceTotalElement = document.getElementById('balance-total');
+const previousBalanceTotalElementString =BalanceTotalElement.innerText;
+const previousBalanceTotal= parseFloat(previousBalanceTotalElementString);
+
+// step-6
+const currentBalanceTotal=previousBalanceTotal + NewDepositAmount;
+// set the balance total
+BalanceTotalElement.innerText =currentBalanceTotal;
 // setp-7
 depositField.value ='';
 
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,25 @@
+document.getElementById('btn-withdraw').addEventListener('click',function(){
+    // step-2:
+    const withdrawFiled = document.getElementById('withdraw-filed');
+    const newWithdrawAmountString = withdrawFiled.value;
+    const newWithdrawAmount =parseFloat(newWithdrawAmountString);
 
+    // step-3
+    const withdrawTotalElement = document.getElementById('withdraw-total');
+    const previouswithdrawTotalString =withdrawTotalElement.innerText;
+    const previouswithdrawTotal = parseFloat(previouswithdrawTotalString);
+//    step-4
+const currentWithdrawTotal = previouswithdrawTotal+newWithdrawAmount;
+withdrawTotalElement.innerText=currentWithdrawTotal;
+//    step-5
+
+const BalanceTotalElement =document.getElementById('balance-total');
+const previousBalanceTotalElementString =BalanceTotalElement.innerText;
+const previousBalanceTotal = parseFloat(previousBalanceTotalElementString)
+// step-6
+const newBalanceTotal =previousBalanceTotal - newWithdrawAmount;
+BalanceTotalElement.innerText = newBalanceTotal;
+
+// step-7
+withdrawFiled.value ="";
+})
