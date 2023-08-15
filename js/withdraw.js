f1c03f5291f59096b195fd241527f9c96d173b63
monira15 <monira.programmer@gmail.com>
2023-08-15 17:12:17 +0600

    revision

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,23 +3,35 @@ document.getElementById('btn-withdraw').addEventListener('click',function(){
     const withdrawFiled = document.getElementById('withdraw-filed');
     const newWithdrawAmountString = withdrawFiled.value;
     const newWithdrawAmount =parseFloat(newWithdrawAmountString);
+    // step-7
+withdrawFiled.value ="";
+    if(isNaN(newWithdrawAmount)){
+        alert('please provaid a number')
+        return;
+    }
 
     // step-3
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previouswithdrawTotalString =withdrawTotalElement.innerText;
     const previouswithdrawTotal = parseFloat(previouswithdrawTotalString);
-//    step-4
-const currentWithdrawTotal = previouswithdrawTotal+newWithdrawAmount;
-withdrawTotalElement.innerText=currentWithdrawTotal;
+
 //    step-5
 
 const BalanceTotalElement =document.getElementById('balance-total');
 const previousBalanceTotalElementString =BalanceTotalElement.innerText;
 const previousBalanceTotal = parseFloat(previousBalanceTotalElementString)
+
+
+if(newWithdrawAmount>previousBalanceTotal){
+    alert('baper bank a ato taka nai')
+    return;
+}
+//    step-4
+const currentWithdrawTotal = previouswithdrawTotal+newWithdrawAmount;
+withdrawTotalElement.innerText=currentWithdrawTotal;
 // step-6
 const newBalanceTotal =previousBalanceTotal - newWithdrawAmount;
 BalanceTotalElement.innerText = newBalanceTotal;
 
-// step-7
-withdrawFiled.value ="";
+
 })
